1e7ee67d507205a842680b5d1e2341994a9041f3
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2025-06-06 02:07:49 +0530

    feat: created user dependency container.

--- users/apps.py
+++ users/apps.py
@@ -4,3 +4,15 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+    inject_container = None
+
+    def ready(self):
+        from users.inject import UsersContainer
+        import users
+
+        self.inject_container = UsersContainer()
+        self.inject_container.wire(
+            packages=[users],
+            modules=[],
+        )
