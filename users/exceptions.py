2e08064a17d952e85d33a5f74abe57a9142553af
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2025-06-07 00:16:19 +0530

    feat: added more user exceptions.

--- users/exceptions.py
+++ users/exceptions.py
@@ -5,5 +5,20 @@ from core.exceptions import OrderlyAuthException
 
 class ErrorAuthenticatingUserWithGoogle(OrderlyAuthException):
     code = "OrderlyAuth_USER_0001"
-    status_code = status.HTTP_401_UNAUTHORIZED
+    status_code = status.HTTP_400_BAD_REQUEST
     default_detail = "Error authenticating user with google"
+
+class CodeOrStateNotFoundException(OrderlyAuthException):
+    code = "OrderlyAuth_USER_0002"
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = "Code or state not found in callback request parameter"
+
+class CSRFCheckFailedException(OrderlyAuthException):
+    code = "OrderlyAuth_USER_0003"
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = "CSRF check failed"
+
+class ErrorObtainingAccessToken(OrderlyAuthException):
+    code = "OrderlyAuth_USER_0003"
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    default_detail = "Failed to obtain access token from Google"
