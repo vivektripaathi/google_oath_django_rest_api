ea233054ab77b8ca443a6fcb01c82f38885321cf
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2025-06-06 22:46:53 +0530

    feat: added google auth view class.

--- users/presentation/views.py
+++ users/presentation/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from rest_framework.views import APIView
+from dependency_injector.wiring import Provide
 
-# Create your views here.
+from users.domain.use_cases.google_raw_login_use_case import GoogleRawLoginUseCase
+
+class GoogleAutView(APIView):
+    def get(
+        self, 
+        request,
+        google_raw_login_use_case: GoogleRawLoginUseCase = Provide["google_raw_login_use_case"],
+    ):
+        authorization_url, state = google_raw_login_use_case.get_authorization_url()
+        request.session["google_oauth2_state"] = state
+        return redirect(authorization_url)
